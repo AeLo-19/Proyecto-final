ff98150a6c5160ac2745eb91de2d8c1759d40fa9
Adrian Lopez <adrianeduardo.lopezo@gmail.com>
2019-12-18 19:49:54 -0400

    pantalla de registro listo (diseño)

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,30 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import emlLogo from "../../img/EML-logo.png";
 import "../../styles/home.scss";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div className="border m-3">
+		<div className="border logo m-3 justify-item-center">
+			<img src={emlLogo} width="280px" height="240px" />
+		</div>
+		<form className="border m-3">
+			<div className="border">
+				<h5>Correo electrónico</h5>
+				<input className="m-2" type="text" name="email" placeholder="Correo Electrónico" />
+				<h5>Contraseña</h5>
+				<input className="m-2" type="text" name="password" placeholder="Contraseña" />
+			</div>
+			<div className="border">
+				{/*debo crear un perfil de prueba con su clave en el 
+                context para comparar y dar acceso a otra ruta o denegar la vaina*/}
+				<button type="button" className="btn btn-primary">
+					Entrar
+				</button>
+				{/* <link> */}
+				<button type="button" className="btn btn-primary">
+					Registrarse
+				</button>
+			</div>
+		</form>
 	</div>
 );
