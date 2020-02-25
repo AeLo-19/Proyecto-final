4fd8918ca51fccf4e87ce6e45bfb2a240a5f4147
Adrian Lopez <adrianeduardo.lopezo@gmail.com>
2020-02-24 22:12:34 -0400

    En teoría todos los fetch listos, falta vincular

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,7 +1,13 @@
+import { push } from "react-router-dom";
 const getState = ({ getStore, getActions, setStore }) => {
 	const ApiUrlRegister = "https://3000-daf31a1e-5d97-4ac9-9ca4-50d448abe4a1.ws-us02.gitpod.io/register";
 	const ApiUrlTratamiento = "https://3000-daf31a1e-5d97-4ac9-9ca4-50d448abe4a1.ws-us02.gitpod.io/tratamiento";
 	const ApiUrlLogin = "https://3000-daf31a1e-5d97-4ac9-9ca4-50d448abe4a1.ws-us02.gitpod.io/login";
+	const ApiUrlAllCitas = "https://3000-daf31a1e-5d97-4ac9-9ca4-50d448abe4a1.ws-us02.gitpod.io/citas";
+	const ApiUrlEspecificUserCitas =
+		"https://3000-daf31a1e-5d97-4ac9-9ca4-50d448abe4a1.ws-us02.gitpod.io/user/" + {} + "/citas";
+	const ApiUrlEspecificCita =
+		"https://3000-daf31a1e-5d97-4ac9-9ca4-50d448abe4a1.ws-us02.gitpod.io/user" + {} + "/citas" + {};
 
 	return {
 		store: {
@@ -14,14 +20,17 @@ const getState = ({ getStore, getActions, setStore }) => {
 					let response = await fetch(ApiUrlLogin, {
 						method: "POST",
 						headers: {
-							Content_Type: "application/json"
+							"Content-Type": "application/json"
 						},
 						body: JSON.stringify(login)
 					});
+					console.log(response.status);
+					console.log(response.json);
 					if (response.ok) {
-						console.log(response.status);
+						push("/home");
 					}
 				} catch (error) {
+					console.log(response.status);
 					console.log("En algo la cagaste");
 					console.log(error);
 				}
@@ -52,17 +61,22 @@ const getState = ({ getStore, getActions, setStore }) => {
 				}
 			},
 			fetchGetTratamiento: async () => {
-				let tratamientos = [];
+				const store = getStore();
+				let tratamientosNuevos = [];
 				try {
 					let response = await fetch(ApiUrlTratamiento, {
 						method: "GET",
 						headers: {
-							"Content-Type": "application/JSOM"
+							"Content-Type": "application/JSON"
 						}
 					});
 					if (reponse.ok) {
-						tratamientos = await response.json();
-					} else if (response.stats > 400) {
+						console.log(tratamientosNuevos);
+						tratamientosNuevos = response.json()
+						setStore({
+							tratamientos = tratamientosNuevos
+						});
+					} else if (response.status > 400) {
 						console.log("no se encontraron tratamientos");
 					}
 				} catch (error) {
@@ -72,6 +86,67 @@ const getState = ({ getStore, getActions, setStore }) => {
 				setStore({
 					tratamientos: tratamientos
 				});
+			},
+			fetchPostCita: async informacion => {
+				const store = getStore();
+				try {
+					let response = await fetch(ApiUrlEspecificUserCitas, {
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json"
+						},
+						body: JSON.stringify(informacion)
+					});
+					console.log(response.status);
+					console.log("Cita creada esperando confirmacion del doctor");
+				} catch (error) {
+					console.log("Verga ya la cagaste otra vez...");
+					console.log(error);
+				}
+			},
+			fetchGetCitas: async () => {
+				const store = getStore();
+				let citasNuevas = [];
+				try {
+					let response = await fetch(ApiUrlAllCitas, {
+						method: "GET",
+						headers: {
+							"Content-Type": "application/JSON"
+						}
+					});
+					if (response.ok){
+						citasNuevas = response.json();
+						setStore({
+							citas = citasNuevas
+						})
+					}
+				} catch (error) {
+					console.log("Se nota que no entiendes muy bien esta vaina");
+					console.log(error);
+				}
+			},
+			fetchPutCita: async aceptado =>{
+				const store = getStore();
+				try{
+					let response = await fetch(ApiUrlEspecificCita, {
+						method: "PUT",
+						headers: {
+							"Content-Type": "application/JSON"
+						}
+					});
+					if (response.ok){
+						console.log(response.json())
+						console.log("Bien hecho has aceptado una cita")
+					} else {
+						console.log("En algo la cagaste... otra vez");
+						console.log(response.status);
+						console.log(response.statusText);
+						console.log(response.json())
+					} 
+					}catch(error){
+						console.log ("Ya no te diré nada chimbo WEBON");
+						console.log(error)
+				}
 			}
 		}
 	};
--- src/js/views/login.js
+++ src/js/views/login.js
@@ -1,27 +1,70 @@
-import React from "react";
-import emlLogo from "../../img/EML-logo.png";
-import "../../styles/home.scss";
+import React, { useState, useEffect, useContext } from "react";
+import emlLogo from "../../img/EML-Isotype.png";
 import { Link } from "react-router-dom";
+import { ModalRegister } from "../component/ModalRegister";
+import { Modal, Button } from "react-bootstrap";
+import { Context } from "../store/appContext";
+import "../../styles/register.scss";
 
-export const Login = () => (
-	<div className="border m-3">
-		<div className="border logo m-3 justify-item-center">
-			<img src={emlLogo} width="280px" height="240px" />
-		</div>
-		<form className="border m-3">
-			<div className="border">
-				<h5>Correo electrónico</h5>
-				<input className="m-2" type="text" name="email" placeholder="Correo Electrónico" />
-				<h5>Contraseña</h5>
-				<input className="m-2" type="text" name="password" placeholder="Contraseña" />
+export const Login = () => {
+	const { store, actions } = useContext(Context);
+
+	const [logIn, setLogIn] = useState({
+		correo: "",
+		clave: ""
+	});
+	var login = {
+		email: logIn.correo,
+		password: logIn.clave
+	};
+	const handleValidation = e => {
+		setLogIn({
+			...logIn,
+			[e.target.name]: e.target.value
+		});
+	};
+	const handleLogIn = e => {
+		e.preventDefault();
+		console.log("validando");
+		if (logIn.correo === "" || logIn.clave === "") {
+			console.log("encontramos un problema usted es medio tonto");
+		} else {
+			console.log("iniciando POST");
+			actions.fetchUserLogin(login);
+		}
+	};
+	return (
+		<div className="border m-3">
+			<div className="border logo m-3 justify-item-center">
+				<img src={emlLogo} width="280px" height="240px" />
 			</div>
-			<div className="border">
-				{/*debo crear un perfil de prueba con su clave en el 
+			<form className="border m-3" onSubmit={handleLogIn}>
+				<div className="border">
+					<h5>Correo electrónico</h5>
+					<input
+						className="m-2"
+						type="text"
+						name="correo"
+						onChange={handleValidation}
+						placeholder="Correo Electrónico"
+					/>
+					<h5>Contraseña</h5>
+					<input
+						className="m-2"
+						type="password"
+						name="clave"
+						onChange={handleValidation}
+						placeholder="Contraseña"
+					/>
+				</div>
+				<div className="border">
+					{/*debo crear un perfil de prueba con su clave en el 
                 context para comparar y dar acceso a otra ruta o denegar la vaina*/}
-				<button type="button" className="btn btn-primary">
-					Entrar
-				</button>
-			</div>
+					<button type="button" className="btn btn-primary" onClick={handleLogIn}>
+						Entrar
+					</button>
+				</div>
+			</form>
 			<div className="borde">
 				<Link to="/register">
 					<button type="button" className="btn btn-primary">
@@ -29,16 +72,6 @@ export const Login = () => (
 					</button>
 				</Link>
 			</div>
-			<div className="col-12">
-				<div className="col-6 inline-block">
-					{/* <p className="text-muted">
-						Si usted trabaja acá haga click
-						<Link to="/login/:loginDr">
-							<p className="text-muted text-primary">AQUÍ</p>
-						</Link>
-					</p> */}
-				</div>
-			</div>
-		</form>
-	</div>
-);
+		</div>
+	);
+};
